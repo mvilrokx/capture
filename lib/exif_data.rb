6f5721f9d49a7309df8ea9a5083661354199ffd3
Mark Vilrokx <mvilrokx@gmail.com>
2012-10-10 21:47:14 -0700

    fixed issue with exifdata module in case pic has no geo data

--- lib/exif_data.rb
+++ lib/exif_data.rb
@@ -1,8 +1,11 @@
 module ExifData
 	def sexagesimal_to_decimal(sexagesimal)
-		sexagesimals = sexagesimal.scan(/\d+/)
-		degrees = sexagesimals[0].to_f + (sexagesimals[1].to_f / 60) + ((sexagesimals[2]+'.'+sexagesimals[2]).to_f / 3600)
-		degrees = degrees * -1 if (sexagesimal.end_with?("W") || sexagesimal.end_with?("S"))
+		degrees = 0
+		if sexagesimal
+			sexagesimals = sexagesimal.scan(/\d+/)
+			degrees = sexagesimals[0].to_f + (sexagesimals[1].to_f / 60) + ((sexagesimals[2]+'.'+sexagesimals[2]).to_f / 3600)
+			degrees = degrees * -1 if (sexagesimal.end_with?("W") || sexagesimal.end_with?("S"))
+		end
 		return degrees
 	end
 end
